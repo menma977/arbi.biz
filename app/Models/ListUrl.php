abcf8dabe9af8e080f6454d941056eef5453d772
SusuTawar <realsusutawar@gmail.com>
2021-01-18 18:07:09 +0700

    list url to database

---
+++ app/Models/ListUrl.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListUrl extends Model
+{
+    use HasFactory;
+}
