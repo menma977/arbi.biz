881cb2ee721147521d6331c69744f822ba61b6a8
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2021-01-19 14:37:48 +0700

    Update HttpController.php

--- app/Http/Controllers/HttpController.php
+++ app/Http/Controllers/HttpController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\ListUrl;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class HttpController extends Controller
+class HttpController
 {
-  public function post($body)
+  static private $key = "";
+
+  /**
+   * @param $action
+   * @param $body
+   * @param bool $needKey
+   * @return array
+   */
+  public static function post($action, $body, $needKey = true)
   {
     $url = ListUrl::where("block", false)->first();
-    if (!$url) return false;
+    if (!$url) {
+      return ['code' => 400, 'message' => 'blocked'];
+    }
+    if ($needKey) {
+      $body["Key"] = self::$key;
+    }
+    $body["a"] = $action;
     $post = Http::asForm()->withHeaders([
       'referer' => 'https://arbi.biz/',
       'origin' => 'https://arbi.biz/'
@@ -19,35 +32,35 @@ class HttpController extends Controller
 
     switch ($post) {
       case $post->serverError():
-        return response()->json(['code' => 500, 'message' => 'server error code 500'], 500);
+        return ['code' => 500, 'message' => 'server error code 500'];
       case $post->clientError():
-        return response()->json(['code' => 401, 'message' => 'client error code 401'], 401);
+        return ['code' => 401, 'message' => 'client error code 401'];
       case str_contains($post->body(), 'IP are blocked for 2 minutes.') === true:
-        return response()->json(['code' => 500, 'message' => 'server has been blocked'], 400);
+        return ['code' => 500, 'message' => 'server has been blocked'];
       case str_contains($post->body(), 'ChanceTooHigh') === true:
-        return response()->json(['code' => 400, 'message' => 'Chance Too High'], 400);
+        return ['code' => 400, 'message' => 'Chance Too High'];
       case str_contains($post->body(), 'ChanceTooLow') === true:
-        return response()->json(['code' => 400, 'message' => 'Chance Too Low'], 400);
+        return ['code' => 400, 'message' => 'Chance Too Low'];
       case str_contains($post->body(), 'InsufficientFunds') === true:
-        return response()->json(['code' => 400, 'message' => 'Insufficient Funds'], 400);
+        return ['code' => 400, 'message' => 'Insufficient Funds'];
       case str_contains($post->body(), 'NoPossibleProfit') === true:
-        return response()->json(['code' => 400, 'message' => 'No Possible Profit'], 400);
+        return ['code' => 400, 'message' => 'No Possible Profit'];
       case str_contains($post->body(), 'MaxPayoutExceeded') === true:
-        return response()->json(['code' => 400, 'message' => 'Max Payout Exceeded'], 400);
+        return ['code' => 400, 'message' => 'Max Payout Exceeded'];
       case str_contains($post->body(), '999doge') === true:
-        return response()->json(['code' => 400, 'message' => 'Invalid request On Server Wait 5 minute to try again'], 400);
+        return ['code' => 400, 'message' => 'Invalid request On Server Wait 5 minute to try again'];
       case str_contains($post->body(), 'error') === true:
-        return response()->json(['code' => 400, 'message' => 'Invalid request'], 400);
+        return ['code' => 400, 'message' => 'Invalid request'];
       case str_contains($post->body(), 'TooFast') === true:
-        return response()->json(['code' => 400, 'message' => 'Too Fast'], 400);
+        return ['code' => 400, 'message' => 'Too Fast'];
       case str_contains($post->body(), 'TooSmall') === true:
-        return response()->json(['code' => 400, 'message' => 'Too Small'], 400);
+        return ['code' => 400, 'message' => 'Too Small'];
       case str_contains($post->body(), 'LoginRequired') === true:
-        return response()->json(['code' => 400, 'message' => 'Login Required'], 400);
+        return ['code' => 400, 'message' => 'Login Required'];
       case str_contains($post->body(), 'InvalidApiKey') === true:
-        return response()->json(['code' => 400, 'message' => 'key you provided is invalid'], 400);
+        return ['code' => 400, 'message' => 'key you provided is invalid'];
       default:
-        return response()->json(['code' => 200, 'data' => $post->json()]);
+        return ['code' => 200, 'data' => $post->json()];
     }
   }
 }
