b1cfb59d0d090c37aa33c26b6e05a10daef846cf
Smity Waber Jager Man Janson <38025704+menma977@users.noreply.github.com>
2021-01-21 17:06:10 +0700

    Update TradingPoolSeeder.php

--- database/seeders/TradingPoolSeeder.php
+++ database/seeders/TradingPoolSeeder.php
@@ -2,17 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\TradingPool;
 use Illuminate\Database\Seeder;
 
 class TradingPoolSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $data = new TradingPool();
+    $data->username = "wallstreet.info";
+    $data->password = "123456+A";
+    $data->wallet = "DFyhesdDdogR5QkdhJ6rwCe7JDEi9tCnfh";
+    $data->Save();
+  }
 }
